ae09757e8a4ca684b9718e4ba346e1adb7a8befe
mahalekshmim <41044027+mahalekshmim@users.noreply.github.com>
2019-01-11 10:46:17 +0530

    Update jenish4.py

--- jenish4.py
+++ jenish4.py
@@ -2,8 +2,8 @@ print("Enter '0' for exit.");
 ch = input("Enter any character: ");
 if ch == '0':
     exit();
-else:
+else
     if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
     	print(ch, "is an alphabet.");
-    else:
+    else
     	print(ch, "is not an alphabet.");
